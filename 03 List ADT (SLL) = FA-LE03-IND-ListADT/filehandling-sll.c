51c20b30f9b361385560b6863ee608be891b5eae
Joshua Sarcol <jjsnippets@gmail.com>
2024-02-05 15:23:54 +0800

    added comments

--- 03 List ADT (SLL) = FA-LE03-IND-ListADT/filehandling-sll.c
+++ 03 List ADT (SLL) = FA-LE03-IND-ListADT/filehandling-sll.c
@@ -5,15 +5,19 @@
 #include "phonebook-sll.h"
 
 #define FILENAME "phonebook.txt"
+    // name of the file
 
 void saveToFile (NODE** start, int* size){
     NODE* currentNode;
 
+    // file opened in write mode
     FILE* fp = fopen(FILENAME, "w");
     rewind(fp);
 
     fprintf(fp, ",lname,mname,fname,num,email\n");
+        // header of the file
 
+    //prints out the rest of the fields in the file
     for(int i = 0; i < *size; i++){
         currentNode = (i == 0 ? *start : currentNode->next);
         fprintf(fp, "%d,%s,%s,%s,%s,%s\n", i, currentNode->lname, currentNode->fname, currentNode->mname,currentNode->num, currentNode->email);
@@ -24,9 +28,11 @@ void saveToFile (NODE** start, int* size){
 
 void loadFromFile (NODE** start, int* size){
 
+    // file opened in read mode
     FILE* fp = fopen(FILENAME, "r");
     char* row = calloc(1000, sizeof(char));
 
+    // if file does not exist
     if (fp == NULL){
        printf("A file named \"%s\" was not found. This will be automatically made after using this program.\n", FILENAME);
        printf("Press any key to continue\n");
@@ -45,13 +51,16 @@ void loadFromFile (NODE** start, int* size){
         return;
     }
 
+    // each time the loop runs, grabs a row
+    // stops when the row is empty (NULL)
     while (fgets(row, 1000, fp) != NULL)
     {
         NODE* addNode = calloc(1, sizeof(NODE));
         addNode->next = NULL;
 
         // grabs individual values from rows
-        strtok(row, ","); // skips index
+        strtok(row, ",");
+            // skips index
         strcpy(addNode->lname, strtok(NULL, ","));
         strcpy(addNode->fname, strtok(NULL, ","));
         strcpy(addNode->mname, strtok(NULL, ","));
@@ -59,6 +68,7 @@ void loadFromFile (NODE** start, int* size){
         strcpy(addNode->email, strtok(NULL, "\n"));
 
         if(*size == 0){
+                // special case when it is the first node to be added
             *start = addNode;
         } else {
             NODE* lastNode = (*start);
@@ -68,7 +78,7 @@ void loadFromFile (NODE** start, int* size){
             }
 
             lastNode->next = addNode;
-        }
+        } // *better implementation: lastNode->next = addNode; lastNode = lastNode->next; without redeclaration of lastNode
 
         (*size)++;
     }
