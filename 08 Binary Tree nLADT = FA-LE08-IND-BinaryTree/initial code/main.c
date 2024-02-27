fc7d959b215fd5a421604064b76c92ea17d34150
Joshua Sarcol <jjsnippets@gmail.com>
2024-02-27 20:33:05 +0800

    add node to binary tree, display single node

--- 08 Binary Tree nLADT = FA-LE08-IND-BinaryTree/initial code/main.c
+++ 08 Binary Tree nLADT = FA-LE08-IND-BinaryTree/initial code/main.c
@@ -9,7 +9,13 @@ typedef struct node {
     struct node* right;
 } NODE;
 
+typedef struct queue {
+    NODE* current;
+    NODE* next;
+} QUEUE;
+
 void displayNode(NODE* toShow);
+void addNode(NODE** root, int data, int size);
 
 void main(){
 
@@ -18,16 +24,50 @@ void main(){
 
     int list[] = {3, 5, 7, 1, 4, 8, 2, 6};
 
-    root->x = 5;
+    addNode(&root, 3, count++);
+    addNode(&root, 4, count++);
+    addNode(&root, 5, count++);
 
     displayNode(root);
+    printf("{%d}", count);
 
+    getche();
 
 }
 
 void displayNode(NODE* toShow){
 
-    printf("[%d %lld %lld %lld", (*toShow).x, toShow, (*toShow).left, (*toShow).right);
+    printf("[%d %ld %ld %ld]\n", toShow->x, toShow, toShow->left, toShow->right);
+
+    printf("Parent: %d\n", toShow->x);
+
+    if (toShow->left)
+        printf("\tLeft child: %d\n", toShow->left->x);
+    if (toShow->right)
+        printf("\tRight child: %d\n", toShow->right->x);
 
 }
 
+void addNode(NODE** root, int data, int size){
+
+    printf("{%d %d}: ", data, size);
+    
+    NODE* newNode = calloc(1, sizeof(NODE));
+    newNode->x = data;
+    displayNode(newNode);
+
+    if (!size){
+        *root = newNode;
+        return;
+    }
+
+    NODE* currentNode = *root;
+
+    for (; size > 3; size >>= 1){
+        (size % 2) ? (currentNode = currentNode->left) : (currentNode->right);
+    }
+
+    (size % 2) ? (currentNode->left = newNode) : (currentNode->right = newNode);
+    printf("\n");
+
+}
